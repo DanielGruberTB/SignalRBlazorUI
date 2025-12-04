e6e07b49be0f56fb176abcfe333e93bed72d5e92
DanielGruberTB <daniel-gruber@outlook.de>
2025-12-04 22:42:54 +0100

    init

---
+++ Pitwallserver/ServerTimeNotifier.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+
+namespace Pitwallserver
+{
+    public class ServerTimeNotifier : BackgroundService
+    {
+        private static readonly TimeSpan Period = TimeSpan.FromSeconds(5);
+        private readonly ILogger<ServerTimeNotifier> _logger;
+        private readonly IHubContext<TelemetryHub, ITelemetryHubClient> _hubContext;
+
+        public ServerTimeNotifier(ILogger<ServerTimeNotifier> logger, IHubContext<TelemetryHub,ITelemetryHubClient> context)
+        {
+            _logger = logger;
+            _hubContext = context;
+
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using var timer = new PeriodicTimer(Period);
+
+            while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
+            {
+                var dateTime = DateTime.Now;
+                _logger.LogInformation("Executing {Service} {Time}", nameof(ServerTimeNotifier), dateTime);
+                await _hubContext.Clients.All.ReceiveNotification($"Server time is {dateTime}");
+            }
+        }
+    }
+}
---
+++ Pitwallserver/TelemetryHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Pitwallserver
+{
+    public class TelemetryHub : Hub<ITelemetryHubClient>
+    {
+        public override Task OnConnectedAsync()
+        {
+            Clients.Client(Context.ConnectionId).ReceiveNotification($"Connected to Telemetry Hub {Context.User?.Identity?.Name}");
+            return base.OnConnectedAsync();
+        }
+    }
+
+    public interface ITelemetryHubClient
+    {
+        Task ReceiveNotification(string message);
+    }
+}
